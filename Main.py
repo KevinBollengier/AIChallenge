9c03b424699036ddb1d96f463ff38599ea53b031
Kevin <kevin.bollengier@student.howest.be>
2017-11-18 14:38:58 -0600

    Training set is printing!!!

--- Main.py
+++ Main.py
@@ -4,16 +4,6 @@ from typing import *
 import nltk
 
 
-def main():
-    negative_tweets = read_tweets(sys.argv[1], "negative")
-    positive_tweets = read_tweets(sys.argv[2], "positive")
-    full_set = strip_tweets(negative_tweets, positive_tweets)
-    get_tweet_words(full_set)
-    word_list = get_tweet_words(full_set)
-    featured_words = get_amount_words(word_list)
-    print(extract_features(negative_tweets[0], featured_words))
-
-
 def read_tweets(filename1: str, sentiment: str) -> List[Tuple[str, str]]:
     """
     Function which reads a json file and returns a List of tuples with a tweet and sentiment.
@@ -30,7 +20,12 @@ def read_tweets(filename1: str, sentiment: str) -> List[Tuple[str, str]]:
     return tweets
 
 
-def strip_tweets(negative_tweets: List[Tuple[str, str]], positive_tweets: List[Tuple[str, str]])->List[Tuple[str, str]]:
+negative_tweets = read_tweets(sys.argv[1], "negative")
+positive_tweets = read_tweets(sys.argv[2], "positive")
+
+
+def strip_tweets(negative_tweets: List[Tuple[str, str]], positive_tweets: List[Tuple[str, str]]) -> List[
+    Tuple[str, str]]:
     """
     Function which strips small words that shouldn't be considered from the data set
     and combines negative and positive tweets.
@@ -40,7 +35,9 @@ def strip_tweets(negative_tweets: List[Tuple[str, str]], positive_tweets: List[T
     """
     tweets = []
     for (words, sentiment) in negative_tweets + positive_tweets:
-        filtered_users_tags = [word.lower() for word in words.split() if len(word) >= 3 and '@' != word[0][:1] and '#' != word[0][:1] and '&' != word[0][:1] and 'http' != word]
+        filtered_users_tags = [word.lower() for word in words.split() if
+                               len(word) >= 3 and '@' != word[0][:1] and '#' != word[0][:1] and '&' != word[0][
+                                                                                                       :1] and 'http' != word]
         filtered_links = [word for word in filtered_users_tags if 'http' not in word]
         filtered_punct = [word.strip(".,?!") for word in filtered_links]
         filtered = [word.replace('...', ' ') for word in filtered_punct]
@@ -48,19 +45,22 @@ def strip_tweets(negative_tweets: List[Tuple[str, str]], positive_tweets: List[T
     return tweets
 
 
-def get_tweet_words(tweet_set: List[Tuple[str, str]])->List[str]:
+tweets = strip_tweets(negative_tweets, positive_tweets)
+
+
+def get_tweet_words(tweet_set: List[Tuple[str, str]]) -> List[str]:
     """
     function that gets all words from a tweet set
     :param tweet_set:
     :return: List of words
     """
     words_list = []
-    for(words, sentiment) in tweet_set:
+    for (words, sentiment) in tweet_set:
         words_list.extend(words)
     return words_list
 
 
-def get_amount_words(wordlist: List[str])->KeysView:
+def get_word_features(wordlist: List[str]) -> KeysView:
     """
     Function that counts the amount of occurrences in a word list and returns the dict with the amount
     :param wordlist: The wordlist
@@ -71,13 +71,16 @@ def get_amount_words(wordlist: List[str])->KeysView:
     return featured_words
 
 
-def extract_features(document, featured_words):
+word_features = get_word_features(get_tweet_words(tweets))
+
+
+def extract_features(document):
     document_words = set(document)
     features = {}
-    for word in featured_words:
+    for word in word_features:
         features['contains(%s)' % word] = (word in document_words)
     return features
 
 
-if __name__ == '__main__':
-    main()
+training_set = nltk.classify.util.apply_features(extract_features, tweets)
+print(training_set)
